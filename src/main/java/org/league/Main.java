ca2917298328d63a7b0a1c4795e29045d2043010
Jacob Riba <jacobriba7@gmail.com>
2022-10-20 23:14:38 +0200

    Initial project  - initial project structure

---
+++ src/main/java/org/league/Main.java
@@ -0,0 +1,15 @@
+package org.league;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String matchScores = in.nextLine();
+
+        System.out.println(matchScores);
+    }
+}
